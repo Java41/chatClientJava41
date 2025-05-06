a51ee7d5d0fba995bf03a8e629422a2f43c375ec
Aleksandr Borodavkin <73429949+AleksandrBorodavkin@users.noreply.github.com>
2025-05-06 21:21:44 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.chatclientjava41 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.chatclientjava41 to javafx.fxml;
+    exports org.example.chatclientjava41;
+}
