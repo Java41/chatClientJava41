7532b3da421cd7cdb55296b1df4266735a1c6b76
ViktorKarsakov <vitakarsak@mail.ru>
2025-06-10 19:13:28 +0700

    ы

--- src/main/java/org/example/chatclientjava41/RegistrationMenuController.java
+++ src/main/java/org/example/chatclientjava41/RegistrationMenuController.java
@@ -18,8 +18,8 @@ public class RegistrationMenuController {
     public void clickMenuAuth(){
         sceneNavigator.setAuth();
     }
-    public void clickRegistration(String email,String password, String date, String firstname, String lastname) {
-        if (email.isEmpty() || password.isEmpty() || date.isEmpty()|| firstname.isEmpty()|| lastname.isEmpty()) {
+    public void clickRegistration(String email,String password, String date) {
+        if (email.isEmpty() || password.isEmpty() || date.isEmpty()) {
             view.setError("Все поля обязательны");
             return;
         }
@@ -34,7 +34,7 @@ public class RegistrationMenuController {
             return;
         }
 
-        String resp = AllResponse.RegistrationUser(email, password, date,firstname,lastname);
+        String resp = AllResponse.RegistrationUser(email, password, date);
         view.setError(resp);
 
         if ("Регистрация прошла успешно".equals(resp)){
